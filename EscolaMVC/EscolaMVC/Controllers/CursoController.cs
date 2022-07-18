265ce7231896c2c739f7f5bfc901c0356c1d9d61
João Pedro <gustavo.s@aluno.senai.br>
2022-07-18 16:23:40 -0300

    atualizar

---
+++ EscolaMVC/EscolaMVC/Controllers/CursoController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using EscolaMVC.Models;
+
+namespace EscolaMVC.Controllers
+{
+    public class CursoController : Controller
+    {
+        private EscolaEntities db = new EscolaEntities();
+
+        // GET: Curso
+        public ActionResult Index()
+        {
+            return View(db.Cursos.ToList());
+        }
+
+        // GET: Curso/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cursos cursos = db.Cursos.Find(id);
+            if (cursos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cursos);
+        }
+
+        // GET: Curso/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Curso/Create
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "CURSOID,CURSODESCRICAO,CURSOCODHABILIDADE,CURSOMODALIDADE")] Cursos cursos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cursos.Add(cursos);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cursos);
+        }
+
+        // GET: Curso/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cursos cursos = db.Cursos.Find(id);
+            if (cursos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cursos);
+        }
+
+        // POST: Curso/Edit/5
+        // Para proteger-se contra ataques de excesso de postagem, ative as propriedades específicas às quais deseja se associar. 
+        // Para obter mais detalhes, confira https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "CURSOID,CURSODESCRICAO,CURSOCODHABILIDADE,CURSOMODALIDADE")] Cursos cursos)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Entry(cursos).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cursos);
+        }
+
+        // GET: Curso/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cursos cursos = db.Cursos.Find(id);
+            if (cursos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cursos);
+        }
+
+        // POST: Curso/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cursos cursos = db.Cursos.Find(id);
+            db.Cursos.Remove(cursos);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ EscolaMVC/EscolaMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EscolaMVC.MvcApplication" Language="C#" %>
